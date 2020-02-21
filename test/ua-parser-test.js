82f244931aa88807b8cb45bf00fe08d27abfe77c
Fernando Pessina <fpessina@inboxlab.com>
2020-02-21 12:27:53 -0300

    Extending ua parser to return complete ua data and adding cache

--- test/ua-parser-test.js
+++ test/ua-parser-test.js
@@ -2,10 +2,14 @@
 
 describe('UA Parser Test', function () {
 
-    let sinon  = require('sinon');
-    let chai   = require('chai');
-    let expect = chai.expect;
-    let assert = chai.assert;
+    const mocha = require('mocha');
+    const describe = mocha.describe;
+    const it = mocha.it;
+    const afterEach = mocha.afterEach;
+    const sinon  = require('sinon');
+    const chai   = require('chai');
+    const expect = chai.expect;
+    const assert = chai.assert;
 
     let UAParser = require('../ua-parser');
 
@@ -75,6 +79,164 @@ describe('UA Parser Test', function () {
         function _testDevice(ua, expected) {
             let value = UAParser.getDevice(ua);
             expect(value).to.equals(expected);
+        }
+    });
+
+    describe('2. Test .getUAData()', () => {
+
+        afterEach(() => {
+            UAParser._clearCacheForTest();
+        });
+
+        it('1. Test for desktop user agents.', () =>{
+            let ua = null;
+
+            // Chrome
+            ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36';
+            _testUserAgent(ua, {
+                deviceType: "desktop",
+                deviceModel: undefined,
+                isMobile: false,
+                browserName: "Chrome",
+                browserVersion: "77.0.3865.120"
+            });
+
+            // Firefox
+            ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0';
+            _testUserAgent(ua, {
+                deviceType: "desktop",
+                deviceModel: undefined,
+                isMobile: false,
+                browserName: "Firefox",
+                browserVersion: "10.0"
+            });
+
+            // Safari
+            ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13 Safari/605.1.15';
+            _testUserAgent(ua, {
+                deviceType: "desktop",
+                deviceModel: undefined,
+                isMobile: false,
+                browserName: "Safari",
+                browserVersion: "13"
+            });
+        });
+
+
+        it('2. Test for mobile user agents.', () =>{
+            let ua = null;
+
+            // Chrome
+            ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/77.0.3865.103 Mobile/15E148 Safari/605.1';
+            _testUserAgent(ua, {
+                browserName: "Chrome",
+                browserVersion: "77.0.3865.103",
+                deviceModel: "iPhone",
+                deviceType: "mobile",
+                isMobile: true
+            });
+
+            // Firefox
+            ua = 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0';
+            _testUserAgent(ua, {
+                browserName: "Firefox",
+                browserVersion: "41.0",
+                deviceModel: undefined,
+                deviceType: "mobile",
+                isMobile: true
+            });
+
+            // Safari
+            ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13 Mobile/15E148 Safari/604.1';
+            _testUserAgent(ua, {
+                browserName: "Mobile Safari",
+                browserVersion: "13",
+                deviceModel: "iPhone",
+                deviceType: "mobile",
+                isMobile: true
+            });
+        });
+
+        it('3. Test for tablet user agents.', () =>{
+            let ua = null;
+
+            // Chrome
+            ua = 'Mozilla/5.0 (Linux; Android 7.0; SM-T827R4 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Safari/537.36';
+            _testUserAgent(ua, {
+                browserName: "Chrome",
+                browserVersion: "60.0.3112.116",
+                deviceModel: "SM-T827R4",
+                deviceType: "tablet",
+                isMobile: true
+            });
+
+            // Firefox
+            ua = 'Mozilla/5.0 (Android 4.4; Tablet; rv:41.0) Gecko/41.0 Firefox/41.0';
+            _testUserAgent(ua, {
+                browserName: "Firefox",
+                browserVersion: "41.0",
+                deviceModel: undefined,
+                deviceType: "tablet",
+                isMobile: true
+            });
+
+            // Safari
+            ua = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148';
+            _testUserAgent(ua, {
+                browserName: "WebKit",
+                browserVersion: "605.1.15",
+                deviceModel: "iPad",
+                deviceType: "tablet",
+                isMobile: true
+            });
+        });
+
+        it('4. Test for other user agents (smart tv, console, wearable or embedded). Expect to return "tablet"', () =>{
+            let ua = null;
+
+            // Console
+            ua = 'Mozilla/5.0 (PLAYSTATION 3; 1.00)';
+            _testUserAgent(ua, {
+                browserName: undefined,
+                browserVersion: undefined,
+                deviceModel: "PLAYSTATION 3",
+                deviceType: "other",
+                isMobile: false
+            });
+        });
+
+        it('5. Test data retrieved from cache for user agents that have already been resolved (_parser.setUA should only be called once)', () => {
+            let ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/77.0.3865.103 Mobile/15E148 Safari/605.1';
+
+            let setUaSpy = sinon.spy(UAParser._getParserForTest(), 'setUA');
+
+            _testUserAgent(ua, {
+                browserName: "Chrome",
+                browserVersion: "77.0.3865.103",
+                deviceModel: "iPhone",
+                deviceType: "mobile",
+                isMobile: true
+            });
+            expect(setUaSpy.calledOnce).to.eql(true);
+
+            _testUserAgent(ua, {
+                browserName: "Chrome",
+                browserVersion: "77.0.3865.103",
+                deviceModel: "iPhone",
+                deviceType: "mobile",
+                isMobile: true
+            });
+            expect(setUaSpy.calledOnce).to.eql(true);
+        });
+
+        /**
+         * @param {String} ua
+         * @param {Object} expected
+         * @private
+         */
+        function _testUserAgent(ua, expected) {
+            let value = UAParser.getUAData(ua);
+            expect(value).to.eql(expected);
         }
     });
 });
--- ua-parser.js
+++ ua-parser.js
@@ -1,6 +1,8 @@
 'use strict';
 
-let UAParserJS = require('ua-parser-js');
+const LRU = require('lru-cache');
+const CACHE_SIZE = 10000;
+const UAParserJS = require('ua-parser-js');
 
 const Device = {
     DESKTOP: 'desktop',
@@ -9,10 +11,11 @@ const Device = {
     OTHER: 'other',
     UNKNOWN: 'unknown'
 };
-
 const OTHER_DEVICES = new Set(['console', 'smarttv', 'wearable', 'embedded']);
+const MOBILE_DEVICES = new Set(['mobile', 'tablet', 'wearable']);
 
 let _parser = new UAParserJS();
+let cache = new LRU(CACHE_SIZE);
 
 class UAParser {
 
@@ -21,21 +24,52 @@ class UAParser {
      * @return {String}
      */
     static getDevice(ua) {
-
         let device = _parser.setUA(ua).getDevice().type;
+        return UAParser._normalizeDeviceType(device);
+    }
 
-        if (device == null) {
+    /**
+     * @param {String} ua
+     * @return {Object}
+     */
+    static getUAData(ua) {
+        let uAData = cache.get(ua);
+        if(uAData != null) {
+            return JSON.parse(uAData);
+        }
+        let deviceData = _parser.setUA(ua).getResult();
+        uAData = {
+            deviceType: this._normalizeDeviceType(deviceData.device.type),
+            deviceModel: deviceData.device.model,
+            isMobile: MOBILE_DEVICES.has(deviceData.device.type),
+            browserName: deviceData.browser.name,
+            browserVersion: deviceData.browser.version
+        };
+        cache.set(ua, JSON.stringify(uAData));
+        return uAData;
+    }
+
+    static _normalizeDeviceType(type) {
+        if (type == null) {
             return Device.DESKTOP;
         }
-        else if (device === Device.MOBILE || device === Device.TABLET) {
-            return device;
+        else if (type === Device.MOBILE || type === Device.TABLET) {
+            return type;
         }
-        else if (OTHER_DEVICES.has(device) === true) {
+        else if (OTHER_DEVICES.has(type) === true) {
             return Device.OTHER;
         }
         else {
             return Device.UNKNOWN;
         }
+    }
+
+    static _clearCacheForTest() {
+        cache.reset();
+    }
+
+    static _getParserForTest() {
+        return _parser;
     }
 }
 
