44af0112084f4fb9443cf33fe6831d5f3bcd8ee9
Leandro Skladnik <lskladnik@inboxlab.com>
2020-03-27 11:12:23 -0300

    Added total and active reqs count

--- req-middleware.js
+++ req-middleware.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const onHeaders = require('on-headers');
+
+let totalReqs = 0;
+let activeReqs = 0;
+
 /* istanbul ignore next */
 function reqMiddleware(req, res, next) {
-
     req.locals = req.locals != null ? req.locals : {};
     req.meta   = req.meta   != null ? req.meta   : {};
 
@@ -10,6 +14,18 @@ function reqMiddleware(req, res, next) {
     // https://stackoverflow.com/questions/19266329/node-js-get-clients-ip
     req.locals.ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
     req.locals.ua = req.headers['user-agent'];
+
+    if(totalReqs === Number.MAX_SAFE_INTEGER){
+        totalReqs = 0; // prevent overflow
+    }
+    activeReqs++;
+    totalReqs++;
+    req.locals.activeReqs = activeReqs;
+    req.locals.totalReqs = totalReqs;
+
+    onHeaders(res, function(){
+        activeReqs -= 1;
+    });
 
     next();
 }
