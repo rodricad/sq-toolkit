bb304931c89a8dfefae20b681b16665d831432b0
Maximiliano Ruani <mruani@inboxlab.com>
2020-04-03 15:25:46 -0300

    - Fixed encoding

--- encoded-object-id.js
+++ encoded-object-id.js
@@ -5,7 +5,8 @@ const ObjectId = require('bson/lib/bson/objectid');
 const CryptoURL = require('./crypto-url');
 const Exception = require('./exception');
 
-const ENCODED_OBJECT_ID_HEX_REGEX = /^[0-9a-fA-F_-]{16}$/;
+const ENCODED_OBJECT_ID_REGEX = /^[0-9a-fA-F_-]{16}$/;
+const ENCODED_OBJECT_ID_ENCODING = 'hex';
 const OBJECT_ID_HEX_REGEX = /^[0-9a-fA-F]{24}$/;
 
 class EncodedObjectId {
@@ -18,7 +19,7 @@ class EncodedObjectId {
         if (this.isObjectId(objectId) === false && this.isObjectIdString(objectId) === false) {
             throw new Exception(Exception.ErrorCode.ERROR_INVALID_PARAMETER, 'Parameter objectId must be valid ObjectId');
         }
-        return CryptoURL.encodeBase64(objectId.toString());
+        return CryptoURL.encodeBase64(objectId.toString(), ENCODED_OBJECT_ID_ENCODING);
     }
 
     /**
@@ -29,7 +30,7 @@ class EncodedObjectId {
         if (EncodedObjectId.isValid(encodedObjectId) === false) {
             throw new Exception(Exception.ErrorCode.ERROR_INVALID_PARAMETER, 'Parameter encodedObjectId must be valid EncodedObjectId string');
         }
-        const str = CryptoURL.decodeBase64(encodedObjectId);
+        const str = CryptoURL.decodeBase64(encodedObjectId, ENCODED_OBJECT_ID_ENCODING);
         return new ObjectId(str);
     }
 
@@ -40,7 +41,7 @@ class EncodedObjectId {
     static isValid(value) {
         return typeof value === 'string'
             && value.length === 16
-            && ENCODED_OBJECT_ID_HEX_REGEX.test(value) === true;
+            && ENCODED_OBJECT_ID_REGEX.test(value) === true;
     }
 
     /**
